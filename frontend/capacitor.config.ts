6ee6aa3804becd5ea12921156c8eea9bd4393ab9
PYB-314 <harryn7768@gmail.com>
2026-05-20 22:50:05 +0900

    update prompt

--- frontend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.pyb314.gurapinganalyzer',
-  appName: '구라핑분석기',
-  webDir: 'out'
-};
-
-export default config;
